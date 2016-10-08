068dd06613d3e5647adcfa59d20cb4a3a2cb8d49
Tim Devlin <timdevlin@gmail.com>
2016-10-08 11:10:37 -0700

    Add more javascript - still not working yet but commiting progress :/

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
 // Back-end Logic
 
+var pandaOut = function(number) {
+  var userNum = [];
+  for (var index = 1; index <= number; index += 1) {
+    if  (index % 15 === 0) {
+     userNum.push("pingpong");
+   } else if (index % 5 === 0) {
+     userNum.push("pong");
+   } else if (index % 3 === 0) {
+     userNum.push("ping");
+   }
+    else {
+      userNum.push(index);
+    }
+   }
+  return userNum
+
+});
+}
 
 
 
 // Front-end Logic
 
-$(function(){
-  $("form#input").submit(function(event) {
+$(function() {
+  $("form#Panda").submit(function(event) {
     event.preventDefault.();
-
-  }
-}
+    var number = parseInt($("input#number").val());
+    var results = pandaOut(number);
+    $("#result").text(result);
+  });
+});
